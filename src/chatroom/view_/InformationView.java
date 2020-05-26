2d5314667859699576a2428c987014e06a04594a
Robin Roth <robin.roth@students.fhnw.ch>
2020-05-26 16:59:14 +0200

    no room information

--- src/chatroom/view_/InformationView.java
+++ src/chatroom/view_/InformationView.java
@@ -4,21 +4,32 @@ import ch.fhnw.richards.topic10_JavaAppTemplate.jat_v2.commonClasses.Translator;
 import chatroom.ServiceLocator;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class InformationView extends Stage {
 
 	public Label info;
-	public HBox box;
-
+	public VBox box;
+	
+	public static Image noRoom = new Image(InformationView.class.getResourceAsStream("no_room.jpg"));
+	public ImageView imageView;
+			
 	public InformationView() {
-		box = new HBox();
-		info = new Label("You are in no Chatroom, Please enter one!");
+		box = new VBox();
+		String h = Double.toString(noRoom.getHeight()) + " " + Double.toString(noRoom.getWidth());
+		info = new Label("Please enter a Chatroom!");
+		imageView = new ImageView(noRoom);
+		imageView.setFitHeight(489);
+		imageView.setFitWidth(363);
 
-		box.getChildren().add(info);
+		box.getChildren().addAll(info, imageView);
 
 		Scene scene = new Scene(box);
+		scene.getStylesheets().add(getClass().getResource("informationview.css").toExternalForm());
 		this.setScene(scene);
 		this.setAlwaysOnTop(true);
 		this.sizeToScene();
